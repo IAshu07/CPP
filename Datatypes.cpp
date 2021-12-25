3817b2602b012152b26055e8636d338520a96cfe
= <=>
2021-12-25 08:23:16 +0530

    Datatypes

---
+++ Datatypes.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+/*
+
+	int a = 123;
+	cout<<a<<endl;
+
+	char b = 'v';
+	cout<<b<<endl;
+
+	bool bl = true;
+	cout<<bl<<endl;
+
+	float f = 1.2;
+	cout<<f<<endl;
+
+	double d = 1.23;
+	cout<<d<<endl;
+
+	int size = sizeof(a);
+	cout<<"Size of a:-"<<size<<endl;
+
+
+	int a = 'a';
+	cout<<a<<endl;
+
+	char ch = 98;
+	cout<<ch<<endl;
+
+	float f = 'c';
+	cout<<f<<endl;
+
+	double d = 100;
+	cout<<d<<endl;
+
+
+	char ch = 123456;
+	cout<<ch<<endl;
+
+
+	unsigned int a = -112;
+	cout<<a<<endl;
+
+	int a = 2/5;
+	cout<<a<<endl;
+
+	cout<<2/5<<endl;
+
+	cout<<2.0/5<<endl;
+
+	int b = 2.0/5;
+	cout<<b<<endl;
+
+	double d = 3.52/2;
+	cout<<d<<endl;
+
+	cout<<3.52/2<<endl;
+
+    int a = 2;
+    int b = 3;
+
+    bool first = (a==b);
+    cout<<first<<endl;
+
+    bool second = (a>b);
+    cout<<second<<endl;
+
+    bool third = (a<b);
+    cout<<third<<endl;
+
+    bool fourth = (a>=b);
+    cout<<fourth<<endl;
+
+    bool fifth = (a<=b);
+    cout<<fifth<<endl;
+
+    bool sixth = (a!=b);
+    cout<<sixth<<endl;	
+
+ */
+	int a = 0;
+	cout<<!a<<endl;
+}
