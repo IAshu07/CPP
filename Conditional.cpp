1bb87004b3680ef19c2eba9a85ceb6dfeba74bab
= <=>
2021-12-26 18:02:42 +0530

    This is About Conditionals

---
+++ Conditional.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//int a;
+	//cin>>a;
+
+	//cout<<"Value of a is "<< a <<endl;
+
+	//If A is Positive
+/*
+	if(a>0) {
+		cout<<"A is Positive"<<endl;
+	}
+
+	else {
+		cout<<"A is Negative"<<endl;
+	}
+
+	int a,b;
+
+	cout<<"Enter the Value of a "<<endl;
+	cin>>a;
+
+	cout<<"Enter the Value of b "<<endl;
+	cin>>b;
+
+	if(a>b)
+	{
+		cout<<"A is Greater"<<endl;
+	}
+	if(b>a)
+	{
+		cout<<"B is Greater"<<endl;
+	}
+*/
+	int a;
+	cout << "Enter the Value of a " << endl;
+	cin>>a;
+
+	if(a>0) {
+		cout<<"A is Positive"<<endl;
+	} 
+	else if (a<0) {
+		cout<<"A is Negative"<<endl;
+	}
+	else {
+		cout<<"A is Zero"<< endl;
+	}
+
+
+
+
+
+}
